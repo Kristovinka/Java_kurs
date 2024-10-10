02a40f39f7e3cde9ed5a294a9faf2b9174573aca
Kristina Tereshchenko <kristovinia@gmail.com>
2024-10-10 23:30:13 +0200

    HW 21

--- src/homework_21/Computer.java
+++ src/homework_21/Computer.java
@@ -7,6 +7,7 @@ public class Computer {
     private Storage storage;
 
     public Computer(Storage storage1) {
+
         this.storage = storage1;
         this.memory = new Memory("SDRAM");
         this.processor = new Processor("i3-323847Y");
