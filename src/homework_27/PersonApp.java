c50ee43d6fce08b2172e3e317363a506e5f27e77
Kristina Tereshchenko <kristovinia@gmail.com>
2024-10-20 22:04:23 +0200

    Java Project

--- src/homework_27/PersonApp.java
+++ src/homework_27/PersonApp.java
@@ -2,9 +2,17 @@ package homework_27;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Person person = new Person("fei@lhh.com", "pasG-wo38rd");
+
+        Person person = new Person("fei@lhh.com", "password");
         System.out.println(person);
 
+        person.setPassword("djdjqhf");
+        person.setEmail("djdjqhf");
+        System.out.println(person);
+
+        person.setPassword("sakla@gmail.com");
+        person.setEmail("Aaa5a-hj");
+        System.out.println(person);
 
 
 
