e43c75ac78aa6a620952f53df1ed69339d45271c
Kristina Tereshchenko <kristovinia@gmail.com>
2024-10-23 23:11:08 +0200

    Java Project

--- src/homework_30/PersonTest.java
+++ src/homework_30/PersonTest.java
@@ -77,6 +77,7 @@ public class PersonTest {
         );
     }
 /////////////////////////////////////////////////////////////////////////
+                    // Homework
 
     @Test
     void testValidPasswordSet() {
