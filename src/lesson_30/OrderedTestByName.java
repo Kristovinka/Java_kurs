85d54349b8151541b9fd9fccea3ac6557c571e94
Kristina Tereshchenko <kristovinia@gmail.com>
2024-10-23 20:31:25 +0200

    Java Project

---
+++ src/lesson_30/OrderedTestByName.java
@@ -0,0 +1,30 @@
+package lesson_30;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+// Тесты будут выполняться по алфавитному порядку названий тестовых методов
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class OrderedTestByName {
+
+    @Test
+    void testB() {
+        System.out.println("Test B");
+    }
+
+    @Test
+    void testA() {
+        System.out.println("Test A");
+    }
+
+    @Test
+    void testC() {
+        System.out.println("Test C");
+    }
+
+    @Test
+    void testD() {
+        System.out.println("Test D");
+    }
+}
--- src/lesson_30/TestExample.java
+++ src/lesson_30/TestExample.java
@@ -1,10 +1,16 @@
-package lesson_29.persons;
+package lesson_30;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class TestExample {
 
@@ -56,6 +62,87 @@ class TestExample {
         System.out.println(fruit);
         assertTrue(fruit.length() > 2);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, -1, 4, 5})
+    void testInts(int value) {
+        System.out.println("current value: " + value);
+        Assertions.assertTrue(value > -5);
+    }
+
+    // CSV - Comma-Separated-Value
+    //Источник данных в формате CSV
+    @ParameterizedTest
+    @CsvSource({"apple, 1", "banana, 2", "cherry, 3"})
+    void testWithCsvSource(String fruit, int rank) {
+        System.out.println(fruit + " | " + (rank + 10));
+        assertTrue(fruit.length() > 4 && rank > 0);
+    }
+
+    // Источник данных файл в формате CSV
+    @ParameterizedTest
+    @CsvFileSource(resources = "data.csv")
+    void testCsvFileSource(String fruit, int rank, boolean flag) {
+        System.out.println(fruit + " | "  + rank + " | " + !flag);
+        assertNotNull(fruit);
+        assertTrue(rank > 0);
+    }
+
+    // Источник данных - метод
+    @ParameterizedTest
+    @MethodSource("testDataFruits")
+    void testMethodSource(String fruit) {
+        System.out.println(fruit);
+        assertNotNull(fruit);
+    }
+
+    static Stream<String> testDataFruits(){
+        return Stream.of("apple", "banana", "cherry");
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDataInt")
+    void testMethodSource2(int value) {
+        System.out.println("current value: " + value);
+        assertNotEquals(100, value);
+    }
+
+    static Stream<Integer> testDataInt() {
+        return Stream.of(1, 2, -1, 4, 5);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDataArguments")
+    void testMethodSource3(int value, int expected, boolean isEquals) {
+        System.out.println(value + " | " + expected + " | " + isEquals);
+        int result = value * value;
+        // assertEquals(expected, result);
+        assertEquals(isEquals, result == expected);
+    }
+
+    static Stream<Arguments> testDataArguments() {
+        return Stream.of(
+                Arguments.of(2, 4, true),
+                Arguments.of(3, 9, true),
+                Arguments.of(4, 15, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testPersonsData")
+    void testPersonsData(Person person, String newEmail) {
+        System.out.println(person);
+        System.out.println(newEmail);
+    }
+
+    static Stream<Arguments> testPersonsData() {
+        Person person = new Person("test@mail.com", "pass1Q!4");
+        return Stream.of(
+                Arguments.of(person, "new1234@mail.com"),
+                Arguments.of(new Person("invalid.mail.com", "password"), "mail.test.com")
+        );
+    }
+
 
 
 }
